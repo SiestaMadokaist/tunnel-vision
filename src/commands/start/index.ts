adedb486e168abd9434cc1a010da05ea19525f55
Rama Patria <rama.patria@moladin.com>
2022-09-24 07:48:04 +0000

    chore: default profile

--- src/commands/start/index.ts
+++ src/commands/start/index.ts
@@ -15,7 +15,7 @@ export async function start(opts: IStart): Promise<void> {
 			rs(dotenv.parse(result) as any);
 		});
 	});
-	process.env.AWS_PROFILE = process.env.AWS_PROFILE ?? Config.AWS_PROFILE;
+	process.env.AWS_PROFILE = process.env.AWS_PROFILE ?? Config.AWS_PROFILE ?? 'tunnelvision';
 	process.env.AWS_REGION = process.env.AWS_REGION ?? Config.AWS_REGION;
 	const hub = new ClientSQSHub({
 		incoming: {
